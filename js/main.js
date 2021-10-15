3b202a437d2811d7be1d723a3201c7042667a037
joseduard <joseduardo.jep@gmail.com>
2021-10-15 12:39:36 +0200

    factorization and object creation and new function to create the random number

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-function play() {
-    // On recupere la saisi de l'utilisateur avec le promp et on le met dans la variable reponse. On transforme la saisie en nombre avec parseInt
-    var reponse = parseInt(prompt("Veuillez saisir un nombre svp"), 10);
-    // C'est pour demander une fois, apres il va changer
-    var solution = Math.round(Math.random() * 500); // On recupére un nombre aleatoire t on l'stock dans la variable solution
-    var compteur = 1; // De façon logique le premier nombre, on l'initialise donc a 1
-    console.log(solution);
+
+
+var game = {
+  // On recupere la saisi de l'utilisateur avec le promp et on le met dans la variable (propriete)nombre recherche. On transforme la saisie en nombre avec parseInt
+  numberSearched: parseInt(prompt("Quel est le nombre a trouver entre 0 et 500 ?"), 10),
+  tryNumber : 1,
+  minNumber:0,
+  maxNumber:200,
+};
+
+function randomNumber(max) {
+  // On recupére un nombre aleatoire t on l'stock dans la variable solution
+  return Math.round(Math.random() * max)
+}
+var solution = randomNumber(game.maxNumber)
+console.log(solution);
+
+function play(reponse, compteur) {
     // tant que la proposition et fause on le permet de reproposer un nouveu nombre != c'est l'inverse de === prend en compte le type de la valeur
-  
     // (proposition !== randomNumber) sera true si c'est different et false si c'est egal
     // (proposition === randomNumber) sera false si c'est different et true si c'est egal
     while (reponse != solution) {
@@ -20,14 +30,13 @@ function play() {
       // on peut mettre le compteur dans le if et le else (conditions et deux fois) mais pour l'ecrire une seule fois on le mets dans la boucle while
       compteur++;
     }
+  
     // Si on sors de la boucle on sait qu'on est trouve le bon nombre et l'alert se declonche
     alert("Vous avez gagné en " + compteur + " essais!!"); // Il sors de la
   }
   
-  
-  
-  do { // d'abord execute l'action, c'est un block de instructions
-      play();
+  do { // d'abord execute l'action (function play();), c'est un block de instructions
+      play(game.numberSearched, game.tryNumber);
   } while (confirm("Désirez-vous rejouer ? ")); // fait la condition repetitive, TANT qu'il est true il va executer do play() et quand il sera false il va sortir de la boucle.
   
   
